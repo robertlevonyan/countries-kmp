188626bb3b70bb309a512842e0057d4088ddb694
Robert Levonyan <bigbob1991@gmail.com>
2024-03-02 11:25:21 +0400

    main toolbar

--- composeApp/src/commonMain/kotlin/com/robertlevonyan/countrieskmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/robertlevonyan/countrieskmp/App.kt
@@ -1,52 +1,22 @@
 package com.robertlevonyan.countrieskmp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.robertlevonyan.countrieskmp.ui.main.MainScreen
 import com.robertlevonyan.countrieskmp.ui.theme.CountriesTheme
-import countrieskmp.composeapp.generated.resources.Res
-import countrieskmp.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
     CountriesTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-//            Button(onClick = { showContent = !showContent }) {
-//                Text(text = "Click me!")
-//            }
-//            AnimatedVisibility(visible = showContent) {
-//                val greeting = remember { Greeting().greet() }
-//                Column(
-//                    modifier = Modifier.fillMaxWidth(),
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    Image(
-//                        painter = painterResource(resource = Res.drawable.compose_multiplatform),
-//                        contentDescription = null,
-//                    )
-//                    Text(text = "Compose: $greeting")
-//                }
-//            }
             MainScreen()
         }
     }
